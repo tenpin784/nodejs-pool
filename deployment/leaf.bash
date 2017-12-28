05b965291332faab82db20bec0d788c7a033f41f
ArqTras <33489188+ArqTras@users.noreply.github.com>
2017-12-28 23:02:40 +0100

    Update leaf.bash

--- deployment/leaf.bash
+++ deployment/leaf.bash
@@ -12,7 +12,7 @@ sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install git python-virtualenv python3-virtualenv curl ntp build-essential screen cmake pkg-config libboost-all-dev libevent-dev libunbound-dev libminiupnpc-dev libunwind8-dev liblzma-dev libldns-dev libexpat1-dev libgtest-dev libzmq3-dev
 cd ~
-git clone https://github.com/3ditguy/nodejs-pool.git  # Change this depending on how the deployment goes.
+git clone https://github.com/arqtras/nodejs-pool.git  # Change this depending on how the deployment goes.
 cd /usr/src/gtest
 sudo cmake .
 sudo make
@@ -23,7 +23,7 @@ cd /usr/local/src
 sudo git clone https://github.com/electroneum/electroneum.git
 cd electroneum
 sudo git checkout
-curl https://raw.githubusercontent.com/3ditguy/nodejs-pool/master/deployment/electroneum_daemon.patch | sudo git apply -v
+curl https://raw.githubusercontent.com/arqtras/nodejs-pool/master/deployment/electroneum_daemon.patch | sudo git apply -v
 sudo make -j$(nproc)
 sudo cp ~/nodejs-pool/deployment/electroneum.service /lib/systemd/system/
 sudo useradd -m electroneumdaemon -d /home/electroneumdaemon
